d70e5eabea91e0e17c7f4bdfa933c27acb1fdfc9
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-12 23:51:41 +0300

    condetional statments

---
+++ basics/11ifelse/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	userAge := 33
+	var result string
+
+	if userAge > 15 {
+		result = "regular user"
+	} else if userAge == 18 {
+		result = "also allowed"
+	} else {
+		result = "under age"
+	}
+
+	fmt.Println("user status: ", result)
+
+	// assing a variable and make a condetion
+	if num := 4; num < 10 {
+		fmt.Println("number is less than 4")
+	} else {
+		fmt.Println("number is greater than 4")
+	}
+}
