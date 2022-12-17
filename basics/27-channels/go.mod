9e2b45b3ddd909eeaa173e502b22719f4daaddfb
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-17 17:41:54 +0300

    channels

---
+++ basics/27-channels/go.mod
@@ -0,0 +1,3 @@
+module github.com/Ahmadkhatib0/channels
+
+go 1.19
---
+++ basics/27-channels/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// channels are a way in which multiple go routines can talk to each other,
+
+func main() {
+	fmt.Println("channels in golang")
+
+	wg := &sync.WaitGroup{}
+	channel := make(chan int, 2)
+
+	// channel <- 4 // push
+	// fmt.Println(<-channel) // throghs an error, the summary of the problem with channels
+	//  is like so : i don't allow you to push a value if no one is listening to me
+
+	wg.Add(2)
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		// this <-chan to prevent putting e.g close(channel) unintentionally before reading from channel
+		// this this <-chane indecate that this function here will only listen to channel, AND WILL NOT MODIFY IT
+
+		// this if conndetion is to handle the NOTE issue
+		val, isChannelOpen := <-channel
+		if isChannelOpen {
+			fmt.Println(val)
+		}
+
+		// fmt.Println(<-channel)
+		// fmt.Println(<-channel)
+		wg.Done()
+	}(channel, wg)
+	go func(ch chan<- int, wg *sync.WaitGroup) {
+		channel <- 4
+		channel <- 8 // with this only: channel := make(chan int),  (or without the second parameter)
+		// this will throgh an error, because in the first func where we listen, we listen only for the first push
+
+		//  NOTE: jf you close the channel without pushing anything, it will return zero to you,
+		// and if you pushed zero to the channel and than closed the channel, it also return zero,
+		// so how to differentiate if the zero is pushed by us, or its the one that is returned be default ?
+
+		close(channel)
+		wg.Done()
+	}(channel, wg)
+
+	wg.Wait()
+}
