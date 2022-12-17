b3b4a63e45bef6bb189eb26cf4305bdd70945722
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-17 16:05:20 +0300

    go routines

---
+++ basics/25-go-routines/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	// "time"
+)
+
+var (
+	wg  sync.WaitGroup // pointer
+	mut sync.Mutex     // pointer
+)
+
+var signals = []string{"test"} // pointer
+
+func main() {
+	// go greeter("hello")
+	// greeter("world")
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1) // 1 becuase its one job
+	}
+
+	wg.Wait() // will make sure that main func will not be ended before finishing the goroutines
+	fmt.Println(signals)
+}
+
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done() // your responsibility to call this done method
+
+	res, err := http.Get(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+
+		fmt.Printf("%d status code for the website %s\n", res.StatusCode, endpoint)
+	}
+}
