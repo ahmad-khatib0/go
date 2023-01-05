98444a4d8e4913ac1c785cb065567c7a0106f116
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-06 00:23:25 +0300

    add books and users components, update header to point to them

---
+++ books-library/frontend/src/components/Books.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-3">Books</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- books-library/frontend/src/router/index.js
+++ books-library/frontend/src/router/index.js
@@ -1,19 +1,43 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Body from "./../components/Body.vue";
-import Login from "./../components/Login.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Body from './../components/Body.vue'
+import Login from './../components/Login.vue'
+import Books from './../components/Books.vue'
+import Book from './../components/Body.vue'
+import BookEdit from './../components/BookEdit.vue'
+import BookAdmin from './../components/BooksAdmin.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Body,
   },
   {
-    path: "/login",
-    name: "Login",
+    path: '/login',
+    name: 'Login',
     component: Login,
   },
-];
+  {
+    path: '/books',
+    name: 'Books',
+    component: Books,
+  },
+  {
+    path: `/books/:bookName`,
+    name: 'Book',
+    component: Book,
+  },
+  {
+    path: '/admin/books',
+    name: 'BookAdmin',
+    component: BookAdmin,
+  },
+  {
+    path: '/admin/books/:bookId',
+    name: 'BookEdit',
+    component: BookEdit,
+  },
+]
 
-const router = createRouter({ history: createWebHistory(), routes });
-export default router;
+const router = createRouter({ history: createWebHistory(), routes })
+export default router
