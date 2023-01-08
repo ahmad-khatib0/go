0a18bf3ef6a08782da5d84ed6796a11cd5e6d3cf
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-08 16:32:49 +0300

    check token on each route guard

--- books-library/backend/cmd/api/routes.go
+++ books-library/backend/cmd/api/routes.go
@@ -22,9 +22,6 @@ func (app *application) routes() http.Handler {
 		MaxAge:           300,
 	}))
 
-	mux.Post("/users/login", app.Login)
-	mux.Post("/users/logout", app.Logout)
-
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.AuthTokenMiddleware)
 		mux.Post("/users", app.AllUsers)
@@ -33,6 +30,10 @@ func (app *application) routes() http.Handler {
 		mux.Post("/users/delete", app.DeleteUser)
 		mux.Post("/log-user-out/{id}", app.LogUserOutAndSetInactive)
 	})
+
+	mux.Post("/users/login", app.Login)
+	mux.Post("/users/logout", app.Logout)
+	mux.Post("/validate-token", app.ValidateToken)
 
 	// mux.Get("/users/add", func(w http.ResponseWriter, r *http.Request) {
 	// 	u := data.User{
