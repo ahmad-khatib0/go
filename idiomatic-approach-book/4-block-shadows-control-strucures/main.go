57b61aa8d091a3d9456a83a1489a5eb340be66c4
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-04 21:50:45 +0300

    switchs

--- idiomatic-approach-book/4-block-shadows-control-strucures/main.go
+++ idiomatic-approach-book/4-block-shadows-control-strucures/main.go
@@ -124,8 +124,41 @@ outer: // note the indentation
 	}
 }
 
+func switchs() {
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			wordLen := len(word)
+			fmt.Println(word, "is exactly the right length:", wordLen)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word!")
+		}
+	}
+
+	// The Case of the Missing Label
+loop:
+	for i := 0; i < 10; i++ {
+		switch {
+		case i%2 == 0:
+			fmt.Println(i, "is even")
+		case i%3 == 0:
+			fmt.Println(i, "is divisible by 3 but not 2")
+		case i%7 == 0:
+			fmt.Println("exit the loop!")
+			break loop
+		default:
+			fmt.Println(i, "is boring")
+		}
+	}
+}
+
 func main() {
 	// shadowingVariables()
 	// ifBlock()
-	loops()
+	// loops()
+	switchs()
 }
