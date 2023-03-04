8b41685cd0a544244735fe52de8b752d3d9263d4
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-05 00:10:05 +0300

    named return values, shadowing named return values

--- idiomatic-approach-book/5-functions/main.go
+++ idiomatic-approach-book/5-functions/main.go
@@ -24,6 +24,15 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(res, reminder) // 2 1
+
+	// Named Return Values
+	x, y, z := namedReturn(5, 2)
+	fmt.Println(x, y, z) // the name that’s used for a
+	// named returned value is local to the function; they don’t enforce any name outside of the function
+
+	fmt.Println(shadowing(5, 2)) // 2 1 nil
+	// The values from the return statement were returned even though they
+	// were never assigned to the named return parameters
 }
 
 func div(nominator, denominator int) int {
@@ -54,6 +63,24 @@ func variadicParameters(base int, vals ...int) []int {
 }
 
 func divAndRemainder(numerator int, denominator int) (int, int, error) {
+	if denominator == 0 {
+		return 0, 0, errors.New("cannot divide by zero")
+	}
+	return numerator / denominator, numerator % denominator, nil
+}
+
+func namedReturn(numerator int, denominator int) (result int, remainder int, err error) {
+	if denominator == 0 {
+		err = errors.New("cannot divide by zero")
+		return result, remainder, err
+	}
+	result, remainder = numerator/denominator, numerator%denominator
+	return result, remainder, err
+}
+
+func shadowing(numerator, denominator int) (result int, remainder int, err error) {
+	// you can shadow a named return value. Be sure that you are assigning to the return value and not to a shadow of it.
+	result, remainder = 20, 30 // shadowing by mistake, but didn't affect the accuracy of the return
 	if denominator == 0 {
 		return 0, 0, errors.New("cannot divide by zero")
 	}
