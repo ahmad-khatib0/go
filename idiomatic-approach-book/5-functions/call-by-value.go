49029898f3a718abece8c407c3f98556267adc51
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-05 22:15:44 +0300

    call be value

---
+++ idiomatic-approach-book/5-functions/call-by-value.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	p := person{}
+	i := 2
+	s := "hello"
+	modifyFails(i, s, p)
+	fmt.Println(i, s, p)
+
+	m := map[int]string{1: "first", 2: "second"}
+	modMap(m)
+	fmt.Println(m) // map[2:hello 3:goodbye]
+	e := []int{1, 2, 3}
+	modSlice(e)
+	fmt.Println(e) // [2 4 6]
+}
+
+type person struct {
+	age  int
+	name string
+}
+
+func modifyFails(i int, s string, p person) {
+	i = i * 2
+	s = "Goodbye"
+	p.name = "Bob"
+}
+
+func modMap(m map[int]string) {
+	m[2] = "hello"
+	m[3] = "goodbye"
+	delete(m, 1)
+}
+func modSlice(s []int) {
+	for k, v := range s {
+		s[k] = v * 2
+	}
+	s = append(s, 10)
+}
