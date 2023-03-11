97d345efe58316f8ad3fb30a4b1c935935936e6b
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-11 15:59:03 +0300

    Code Your Methods for nil Instances

--- idiomatic-approach-book/7-types-methods-interfaces/main.go
+++ idiomatic-approach-book/7-types-methods-interfaces/main.go
@@ -22,6 +22,8 @@ func main() {
 	fmt.Println(c.String()) // total: 0, last updated: 0001-01-01 00:00:00 +0000 UTC
 	c.Increment()
 	fmt.Println(c.String()) // total: 1, last updated: 2009-11-10 23:00:00 +0000 UTC m=+0.000000001
+
+	methodsForNilInstances()
 }
 
 func primitiveTypes() {
@@ -54,4 +56,49 @@ func (c *Counter) Increment() {
 }
 func (c *Counter) String() string {
 	return fmt.Sprintf("total: %d last updated %v", c.total, c.lastUpdated)
+}
+
+// Code Your Methods for nil Instances
+type IntTree struct {
+	val         int
+	left, right *IntTree
+}
+
+func (it *IntTree) Insert(val int) *IntTree {
+	if it == nil {
+		return &IntTree{val: val}
+	}
+	if val < it.val {
+		it.left = it.left.Insert(val)
+	} else if val > it.val {
+		it.right = it.right.Insert(val)
+	}
+	return it
+}
+
+func (it *IntTree) Contains(val int) bool {
+	switch {
+	case it == nil:
+		return false
+	case val < it.val:
+		return it.left.Contains(val)
+	case val > it.val:
+		return it.right.Contains(val)
+	default:
+		return true
+	}
+}
+
+// The Contains method doesn’t modify the *IntTree, but it is declared with a pointer receiver. This demonstrates
+// the rule mentioned above about supporting a nil receiver. A method with a value receiver ( can’t check for nil )
+// and as mentioned earlier, panics if invoked with a nil receiver.
+func methodsForNilInstances() {
+	var it *IntTree
+	it = it.Insert(5)
+	it = it.Insert(3)
+	it = it.Insert(10)
+	it = it.Insert(2)
+	fmt.Println(it.Contains(2))  // true
+	fmt.Println(it.Contains(12)) // false
+
 }
