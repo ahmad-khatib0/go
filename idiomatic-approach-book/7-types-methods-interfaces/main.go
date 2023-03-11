45eb5a1cb67d135b10db258cbe8cdf991ff56144
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-11 15:09:48 +0300

    Method declarations and invocation

--- idiomatic-approach-book/7-types-methods-interfaces/main.go
+++ idiomatic-approach-book/7-types-methods-interfaces/main.go
@@ -1,12 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
 	// you can use any primitive type or compound type literal to define a concrete type
 	primitiveTypes()
+
+	// Method invocations
+	p := Person{
+		FirstName: "John",
+		LastName:  "Doe",
+		Age:       33,
+	}
+	output := p.String()
+	fmt.Println(output) // John Doe, age 33
 }
 
 func primitiveTypes() {
 	type Score int
 	type Converter func(string) Score
 	type TeamScores map[string]Score
+}
+
+// Methods
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+// Method declarations
+func (p Person) String() string {
+	return fmt.Sprintf("%s %s, age %d", p.FirstName, p.LastName, p.Age)
 }
