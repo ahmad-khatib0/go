6ff2a65fd4137b9d7a2050151638ed6168553439
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-15 01:32:22 +0300

    Displaying one page, add air for hot reloading

--- fidget-spinners-store/cmd/web/handlers.go
+++ fidget-spinners-store/cmd/web/handlers.go
@@ -3,5 +3,8 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("hit the VirtualTerminal handler")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+
 }
