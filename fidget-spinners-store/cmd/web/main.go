daea0bc2ac66a98289ae61b1a56d66539ee2d51b
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-19 02:45:38 +0300

    Creating a database table for items for sale

--- fidget-spinners-store/cmd/web/main.go
+++ fidget-spinners-store/cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/Ahmadkhatib0/go/fidget-spinners-store/internal/driver"
+	"github.com/Ahmadkhatib0/go/fidget-spinners-store/internal/models"
 )
 
 const version = "1.0.0"
@@ -34,6 +35,7 @@ type application struct {
 	errorLog      *log.Logger
 	templateCache map[string]*template.Template
 	version       string
+	DB            models.DBModel
 }
 
 func (app *application) serve() error {
@@ -81,6 +83,7 @@ func main() {
 		errorLog:      errorLog,
 		templateCache: tc,
 		version:       version,
+		DB:            models.DBModel{DB: conn},
 	}
 
 	err = app.serve()
