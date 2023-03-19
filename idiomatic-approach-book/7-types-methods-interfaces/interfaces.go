754e5b9f3af61f169daf4cc9e41670acdc27a76e
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-19 21:14:06 +0300

    nil interfaces

--- idiomatic-approach-book/7-types-methods-interfaces/interfaces.go
+++ idiomatic-approach-book/7-types-methods-interfaces/interfaces.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
 	fmt.Println("interfaces")
 	c := Client{L: LogicProvider{}}
 	c.Progeram()
+
+	theAnyType()
 }
 
 type LogicProvider struct{}
@@ -49,4 +55,55 @@ type Closer interface {
 type ReadCloser interface {
 	Reader
 	Closer
+}
+
+// Interfaces and nil
+func interfacesAndNil() {
+	// In order for an interface to be considered nil both the type and the value must be nil
+	var s *string
+	fmt.Println(s == nil) // prints true
+	var i interface{}
+	fmt.Println(i == nil) // prints true
+	i = s
+	fmt.Println(i == nil) // prints false
+}
+
+// the Any type by using an (Empty Interface)
+func theAnyType() {
+	var i interface{}
+	i = 20
+	i = "hello"
+	i = struct {
+		FirstName string
+		LastName  string
+	}{"Fred", "Fredson"}
+	fmt.Println(i) // {Fred Fredson}
+}
+
+// using interface  is as way to store a value in a user-created data structure.
+// This is due to Go’s current lack of user-defined generics
+func fileReader(filename string) error {
+	data := map[string]interface{}{}
+	contents, err := ioutil.ReadFile("testdata/sample.json")
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(contents, &data)
+}
+
+type LinkedList struct {
+	Value interface{}
+	Next  *LinkedList
+}
+
+func (ll *LinkedList) Insert(pos int, val interface{}) *LinkedList {
+	if ll == nil || pos == 0 {
+		return &LinkedList{
+			Value: val,
+			Next:  ll,
+		}
+	}
+
+	ll.Next = ll.Next.Insert(pos-1, val)
+	return ll
 }
