0ea43db277c0aea8389cafb0331d5158ff5bdc7e
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-25 14:08:19 +0300

    Errors are values *

--- idiomatic-approach-book/8-errors/main.go
+++ idiomatic-approach-book/8-errors/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"archive/zip"
 	"bytes"
+	"errors"
 	"fmt"
 )
 
@@ -14,6 +15,20 @@ func main() {
 	fmt.Println(res)
 
 	zipReader()
+
+	errorsAreValues("3345", "pwd", "filename")
+
+	err = uninitializedInstance(true)
+	fmt.Println(err != nil) // prints true , which is expected
+	err = uninitializedInstance(false)
+	fmt.Println(err != nil) // prints false!, which is wrong
+
+	//  ╔══════════════════════════════════════════════════════════════════════════════════════════╗
+	//  ║   The reason why err is non-nil is that error is a interface.  for an interface to be    ║
+	//  ║      considered nil, both the underlying type and the underlying value must be nil.      ║
+	//  ║ Whether or not genErr is a pointer, the underlying type part of the interface is not nil ║
+	//  ╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
 }
 
 func doubleEven(i int) (int, error) {
@@ -24,8 +39,11 @@ func doubleEven(i int) (int, error) {
 	return i * 2, nil
 }
 
-// *********************************   Sentinel errors *********************************
-// Sentinel errors are usually used to indicate that you cannot start or continue processing
+//     *********************************   Sentinel errors *********************************
+//  ▲
+//  █ Sentinel errors are usually used to indicate that you cannot start or continue processing
+//  ▼
+
 func zipReader() {
 	data := []byte("this is not a zip file or data")
 	notAZipFile := bytes.NewReader(data)
@@ -34,4 +52,57 @@ func zipReader() {
 		fmt.Println("not a zip file")
 	}
 
+}
+
+// ********************************* Errors are values *********************************
+type Status int
+
+const (
+	InvalidLogin Status = iota + 1
+	NotFound
+)
+
+type StatusErr struct {
+	Status  Status
+	Message string
+}
+
+func (se StatusErr) Error() string {
+	return se.Message
+}
+
+func errorsAreValues(uid, pwd, file string) ([]byte, error) {
+	err := login(uid, pwd)
+	if err != nil {
+		return nil, StatusErr{
+			Status:  InvalidLogin,
+			Message: fmt.Sprintf("invalid credentials for user %s", uid),
+		}
+	}
+	data, err := getData(file)
+	if err != nil {
+		return nil, StatusErr{
+			Status:  NotFound,
+			Message: fmt.Sprintf("file %s not found", file),
+		}
+	}
+	return data, nil
+}
+
+func login(uid, pwd string) error {
+	return nil
+}
+func getData(file string) ([]byte, error) {
+	return nil, errors.New("file not found")
+}
+
+// DON'T use this pattern  (DON’T RETURN AN UNINITIALIZED INSTANCE)
+func uninitializedInstance(flag bool) error {
+	var genErr StatusErr
+	if flag {
+		genErr = StatusErr{
+			Status: NotFound,
+		}
+	}
+	return genErr
 }
