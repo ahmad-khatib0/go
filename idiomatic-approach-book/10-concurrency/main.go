6eb66e81c0ed88c270f07516f9385381a301f095
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-27 00:00:10 +0300

    pipelines

--- idiomatic-approach-book/10-concurrency/main.go
+++ idiomatic-approach-book/10-concurrency/main.go
@@ -7,13 +7,15 @@ import (
 
 func main() {
 
-	selectGoRoutine()
+	// selectGoRoutine()
 
-	deadLockingGoroutines()
+	// deadLockingGoroutines()
 
 	// infiniteRoutine()
 
-	doneChannel()
+	// doneChannel()
+
+	pipelines()
 }
 
 func process(val int) int {
@@ -140,4 +142,46 @@ func doneChannel() {
 
 	time.Sleep(time.Second * 3)
 	close(done)
+}
+
+// ********************************      Pipelines     *********************************
+
+func sliceToChannel(nums []int) <-chan int { // read only channel
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			fmt.Println("i'm gonna wait 2 sec before writing to the channel")
+			time.Sleep(time.Second * 2)
+			out <- n
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			fmt.Println("i have received the result from the channel")
+			out <- n
+		}
+		close(out)
+	}()
+
+	return out // the go routine could be sending data to the out channel even thogh we Returned from the function
+}
+
+func pipelines() {
+	nums := []int{1, 3, 4, 5, 6} // The Input
+
+	dataChannel := sliceToChannel(nums) // Stage 1
+
+	finalChannel := sq(dataChannel) // Stage 2
+
+	// Stage 3
+	for n := range finalChannel {
+		fmt.Println(n)
+	}
 }
