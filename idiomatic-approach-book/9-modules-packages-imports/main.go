032af0785189edc5e00c626c00d66a1dcf5b41e1
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-26 14:58:26 +0300

    Gracefully Renaming and Reorganizing Your API

--- idiomatic-approach-book/9-modules-packages-imports/main.go
+++ idiomatic-approach-book/9-modules-packages-imports/main.go
@@ -22,3 +22,34 @@ func seedRand() *rand.Rand {
 		rand.New(rand.NewSource(int64(binary.LittleEndian.Uint64(b[:]))))
 	return r
 }
+
+// ***************************  Gracefully Renaming and Reorganizing Your API  *************************
+
+type Foo struct {
+	x int
+	S string
+}
+
+func (f Foo) Hello() string {
+	return "hello"
+}
+func (f Foo) goodbye() string {
+	return "goodbye"
+}
+
+type Bar = Foo //  to access Foo by the name Bar
+
+func MakeBar() Bar {
+	bar := Bar{
+		x: 20,
+		S: "Hello",
+	}
+	var f Foo = bar
+	fmt.Println(f.Hello())
+	return bar
+}
+
+func renamingTypes() {
+	type Bar = Foo //  to access Foo by the name Bar
+
+}
