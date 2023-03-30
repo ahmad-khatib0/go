8e1aa47ea142d72a4c05a05368ff56b4c845f755
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-03-31 02:32:58 +0300

    Using WaitGroups

--- idiomatic-approach-book/10-concurrency/more.go
+++ idiomatic-approach-book/10-concurrency/more.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
@@ -32,4 +33,53 @@ func timeLimit() (int, error) {
 func doSomeWork() (int, error) {
 	time.Sleep(time.Second * 1)
 	return 2, nil
+}
+
+// *********************************   Using WaitGroups  *********************************
+func waitGroups() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		doSomeWork1()
+	}()
+	go func() {
+		defer wg.Done()
+		doSomeWork2()
+	}()
+	go func() {
+		defer wg.Done()
+		doSomeWork3()
+	}()
+
+	wg.Wait()
+}
+
+func doSomeWork1() {}
+func doSomeWork2() {}
+func doSomeWork3() {}
+
+// *********************************   Using WaitGroups  *********************************
+func processAndGather(in <-chan int, processor func(int) int, num int) []int {
+	out := make(chan int, num)
+	var wg sync.WaitGroup
+	wg.Add(num)
+	for i := 0; i < num; i++ {
+		go func() {
+			defer wg.Done()
+			for v := range in {
+				//  The for-range channel loop exits when out is closed and the buffer is empty
+				out <- processor(v)
+			}
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	var result []int
+	for v := range out {
+		result = append(result, v)
+	}
+	return result
 }
