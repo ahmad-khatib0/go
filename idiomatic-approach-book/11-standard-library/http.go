5b9f34619b70419c266e73b5b0c1df6211d18cc2
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-01 18:43:13 +0300

    http server

--- idiomatic-approach-book/11-standard-library/http.go
+++ idiomatic-approach-book/11-standard-library/http.go
@@ -12,6 +12,7 @@ func main() {
 	httpRequest()
 }
 
+// ***************** HTTP Client
 func httpRequest() {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "https://jsonplaceholder.typicode.com/todos/1", nil)
 	if err != nil {
@@ -46,4 +47,42 @@ func httpRequest() {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", data) // { UserID:1 ID:1 Title:delectus aut autem Completed:false }
+}
+
+// ***************** HTTP Server
+type HelloHandler struct{}
+
+func (hh HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!\n"))
+
+	s := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      HelloHandler{},
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			panic(err)
+		}
+	}
+}
+
+func muxServe() {
+
+	person := http.NewServeMux()
+	person.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("greetings!\n"))
+	})
+
+	dog := http.NewServeMux()
+	dog.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("good puppy!\n"))
+	})
+
+	mux := http.NewServeMux()
+	mux.Handle("/person/", http.StripPrefix("/person", person))
+	mux.Handle("/dog/", http.StripPrefix("/dog", dog))
 }
