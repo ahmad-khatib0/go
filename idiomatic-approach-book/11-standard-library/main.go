e252b6c471ed0bff1a4739096c227b0ff516d453
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-01 10:05:31 +0300

    time

--- idiomatic-approach-book/11-standard-library/main.go
+++ idiomatic-approach-book/11-standard-library/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
+
+	goEssentialDate() // March 13, 2016 at 12:00:00AM +0000
 
 }
 
@@ -49,4 +53,14 @@ func buildGZipReader(fileName string) (*gzip.Reader, func(), error) {
 		gr.Close()
 		r.Close()
 	}, nil
+}
+
+// ********************************* Time *********************************
+func goEssentialDate() error {
+	t, err := time.Parse("2006-02-01 15:04:05 -0700", "2016-13-03 00:00:00 +0000")
+	if err != nil {
+		return err
+	}
+	fmt.Println(t.Format("January 2, 2006 at 3:04:05PM MST")) // => March 13, 2016 at 12:00:00AM +0000
+	return nil
 }
