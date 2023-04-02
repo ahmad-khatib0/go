55f221769f331aeb398699edd62833dc8ec83d52
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-02 13:12:21 +0300

    testing public api

---
+++ idiomatic-approach-book/13-testing/adder/adder_public_test.go
@@ -0,0 +1,14 @@
+package adder_test
+
+import (
+	"testing"
+
+	"github.com/Ahmadkhatib0/go/idiomatic-approach-book/testing/adder"
+)
+
+func TestAddNumbers(t *testing.T) {
+	result := adder.AddNumbers(2, 3)
+	if result != 5 {
+		t.Error("incorrect result: expected 5, got", result)
+	}
+}
