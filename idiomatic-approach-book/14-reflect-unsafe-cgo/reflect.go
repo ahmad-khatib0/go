486d659daf3658f450c53a2646e62832547118f5
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-02 16:33:41 +0300

    valueOf

--- idiomatic-approach-book/14-reflect-unsafe-cgo/reflect.go
+++ idiomatic-approach-book/14-reflect-unsafe-cgo/reflect.go
@@ -58,3 +58,21 @@ func reflectOnStruct() {
 }
 
 //******************************************** Values ********************************************
+
+func valueOf() {
+	s := []string{"a", "b", "c"}
+	sv := reflect.ValueOf(s)        // sv is of type reflect.Value
+	s2 := sv.Interface().([]string) // s2 is of type []string
+	fmt.Println(s2)
+
+	i := 10
+	iv := reflect.ValueOf(&i)
+	ivv := iv.Elem()
+	ivv.SetInt(20)
+	fmt.Println(i) // prints 20
+}
+
+func changeIntReflect(i *int) {
+	iv := reflect.ValueOf(i)
+	iv.Elem().SetInt(20)
+}
