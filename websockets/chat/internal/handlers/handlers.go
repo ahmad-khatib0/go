e44fd954044afadf43630b93af5145fb57478388
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-16 15:07:17 +0300

    Sending messages

--- websockets/chat/internal/handlers/handlers.go
+++ websockets/chat/internal/handlers/handlers.go
@@ -121,6 +121,10 @@ func ListenToWsChannel() {
 			response.ConnectedUsers = users
 			broadcastToAll(response)
 
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
+			broadcastToAll(response)
 		}
 	}
 }
@@ -128,7 +132,9 @@ func ListenToWsChannel() {
 func getUserList() []string {
 	var userList []string
 	for _, x := range clients {
-		userList = append(userList, x)
+		if x != "" {
+			userList = append(userList, x)
+		}
 	}
 	sort.Strings(userList)
 	return userList
