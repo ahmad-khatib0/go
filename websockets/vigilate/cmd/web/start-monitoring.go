2611b411c270fb02b845da902ca6c035abbedece
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-21 19:46:10 +0300

    Scheduler

--- websockets/vigilate/cmd/web/start-monitoring.go
+++ websockets/vigilate/cmd/web/start-monitoring.go
@@ -1,24 +1,40 @@
 package main
 
+import "log"
+
 // job is the unit of work to be performed
 type job struct {
 	HostServiceID int
 }
 
-// Run runs the scheduler
+// Run runs the scheduled job
 func (j job) Run() {
-
+	repo.ScheduledCheck(j.HostServiceID)
 }
 
 // startMonitoring starts the monitoring process
 func startMonitoring() {
 	if preferenceMap["monitoring_live"] == "1" {
+		log.Println("*************** starting monitor")
+		// trigger a message to broadcast to all clients that app is starting to monitor
 		data := make(map[string]string)
-		data["message"] = "starting"
-
-		// TODO trigger a message to broadcast to all clients that app is starting to monitor
+		data["message"] = "Monitoring is starting..."
+		err := app.WsClient.Trigger("public-channel", "app-starting", data)
+		if err != nil {
+			log.Println(err)
+		}
 
 		// get all of the services that we want to monitor
+		servicesToMonitor, err := repo.DB.GetServicesToMonitor()
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println("Length of servicesToMonitor is", len(servicesToMonitor))
+
+		for _, x := range servicesToMonitor {
+			log.Println("*** Service to monitor on", x.HostName, "is", x.Service.ServiceName)
+		}
 
 		// range through the services
 
