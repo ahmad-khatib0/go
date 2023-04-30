44b993de677c45902df18a6d71d05733f56afa36
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-30 16:18:28 +0300

    Getting started with the Producer - the pizzeria function

--- concurrency/2-race_conditions-mutexes-channels/producer-consumer/main.go
+++ concurrency/2-race_conditions-mutexes-channels/producer-consumer/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // +------------------------------------------------------------------------------------------------+
 // | with mutex , once we use the go keyword, we can not to talk to that routine , we can only wait |
 // | for it to finishو, but with channels we can to talk to that routine, and exchange data with it |
@@ -9,7 +16,7 @@ const NumberOfPizzas = 10
 
 var PizzasMade, PizzasFailed, total int
 
-type Porducer struct {
+type Producer struct {
 	data chan PizzaOrder
 	quit chan chan error // one channel that holds a channel of errors
 }
@@ -20,15 +27,41 @@ type PizzaOrder struct {
 	success     bool
 }
 
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch // will return nil if channel closed successfully , and error if any
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	// keep track of which pizza we are making
+
+	// run forever or until we receive a quit notification
+	// try to make pizzas
+	for {
+		// try to make a pizza
+		// decision
+	}
+
+}
+
 func main() {
 
 	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print out a message
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
 
 	// create a producer
+	pizzaJop := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
 
 	// run the producer in the background
+	go pizzeria(pizzaJop)
 
 	// create and run consumer
 
