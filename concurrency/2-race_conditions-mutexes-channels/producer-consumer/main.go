dfe288686087a8bbceb768cc3ac6ff1c6cb7a099
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-04-30 16:02:27 +0300

    Producer/Consumer - Using Channels for the first time

--- concurrency/2-race_conditions-mutexes-channels/producer-consumer/main.go
+++ concurrency/2-race_conditions-mutexes-channels/producer-consumer/main.go
@@ -1,5 +1,36 @@
 package main
 
+// +------------------------------------------------------------------------------------------------+
+// | with mutex , once we use the go keyword, we can not to talk to that routine , we can only wait |
+// | for it to finishو, but with channels we can to talk to that routine, and exchange data with it |
+// +------------------------------------------------------------------------------------------------+
+
+const NumberOfPizzas = 10
+
+var PizzasMade, PizzasFailed, total int
+
+type Porducer struct {
+	data chan PizzaOrder
+	quit chan chan error // one channel that holds a channel of errors
+}
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
 func main() {
 
+	// seed the random number generator
+
+	// print out a message
+
+	// create a producer
+
+	// run the producer in the background
+
+	// create and run consumer
+
+	// print out the ending message
 }
