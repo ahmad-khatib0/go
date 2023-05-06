fa15bba71fb0c0303dff56bc6b92533632698afd
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-07 02:00:56 +0300

    quick fix

--- concurrency/4-channels/sleeping-barber/barbershop.go
+++ concurrency/4-channels/sleeping-barber/barbershop.go
@@ -16,17 +16,16 @@ type BarberShop struct {
 }
 
 func (shop *BarberShop) addBarber(barber string) {
-
 	shop.NumberOfBarbers++
 
 	go func() {
 		isSleeping := false
-		color.Yellow("%s goes to waiting room to check for clients.", barber)
+		color.Yellow("%s goes to the waiting room to check for clients.", barber)
 
 		for {
-			// if there are no clients , the barber goes to sleep
+			// if there are no clients, the barber goes to sleep
 			if len(shop.ClientsChan) == 0 {
-				color.Yellow("there is nothing to do, so the %s takes a nap", barber)
+				color.Yellow("There is nothing to do, so %s takes a nap.", barber)
 				isSleeping = true
 			}
 
@@ -37,53 +36,54 @@ func (shop *BarberShop) addBarber(barber string) {
 					color.Yellow("%s wakes %s up.", client, barber)
 					isSleeping = false
 				}
+				// cut hair
 				shop.cutHair(barber, client)
 			} else {
-				// shop is closed, so send the barber home, and close this routine
+				// shop is closed, so send the barber home and close this goroutine
 				shop.sendBarberHome(barber)
 				return
 			}
 		}
-
 	}()
 }
 
 func (shop *BarberShop) cutHair(barber, client string) {
-	color.Green("%s is cutting %s's hair", barber, client)
+	color.Green("%s is cutting %s's hair.", barber, client)
 	time.Sleep(shop.HairCutDuration)
-	color.Green("%s is finished cutting %s's hair ", barber, client)
+	color.Green("%s is finished cutting %s's hair.", barber, client)
 }
 
 func (shop *BarberShop) sendBarberHome(barber string) {
-	color.Cyan("%s is going home", barber)
-
+	color.Cyan("%s is going home.", barber)
 	shop.BarbersDoneChan <- true
 }
 
-func (shop *BarberShop) CloseShopForDay() {
-	color.Cyan("Closing shop for the day")
+func (shop *BarberShop) closeShopForDay() {
+	color.Cyan("Closing shop for the day.")
+
 	close(shop.ClientsChan)
 	shop.Open = false
 
 	for a := 1; a <= shop.NumberOfBarbers; a++ {
-		<-shop.BarbersDoneChan // CloseShopForDay
+		<-shop.BarbersDoneChan
 	}
 
 	close(shop.BarbersDoneChan)
 
-	color.Green("-------------------------------------------------------------")
-	color.Green("The barber now is closed for the day, and every one gone home")
+	color.Green("---------------------------------------------------------------------")
+	color.Green("The barbershop is now closed for the day, and everyone has gone home.")
 }
 
-func (shop *BarberShop) addClinet(client string) {
-	color.Green("*** %s arrives", client)
+func (shop *BarberShop) addClient(client string) {
+	// print out a message
+	color.Green("*** %s arrives!", client)
 
 	if shop.Open {
 		select {
 		case shop.ClientsChan <- client:
-			color.Blue("%s takes a seat in the waiting room", client)
+			color.Yellow("%s takes a seat in the waiting room.", client)
 		default:
-			color.Red("the waiting room is fullt, so the %s leaves", client)
+			color.Red("The waiting room is full, so %s leaves.", client)
 		}
 	} else {
 		color.Red("The shop is already closed, so %s leaves!", client)
