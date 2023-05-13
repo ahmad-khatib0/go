21cc4f71b143a9be875cce2e609bc84022768938
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-14 00:46:13 +0300

    Authorize with Access Control Lists

--- distributed-services/proglog/internal/server/server.go
+++ distributed-services/proglog/internal/server/server.go
@@ -4,12 +4,25 @@ import (
 	"context"
 
 	api "github.com/Ahmadkhatib0/go/distributed-services/proglog/api/v1"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
 )
 
 type Config struct {
-	CommitLog CommitLog
+	CommitLog  CommitLog
+	Authorizer Authorizer
 }
+
+const (
+	objectWildcard = "*"
+	produceAction  = "produce"
+	consumeAction  = "consume"
+)
 
 var _ api.LogServer = (*grpcServer)(nil)
 
@@ -25,20 +38,32 @@ func newgrpcServer(config *Config) (srv *grpcServer, err error) {
 	return srv, nil
 }
 
-func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (
-	*grpc.Server,
-	error,
-) {
+func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (*grpc.Server, error) {
+	opts = append(opts,
+		grpc.StreamInterceptor(
+			grpc_middleware.ChainStreamServer(grpc_auth.StreamServerInterceptor(authenticate)),
+		),
+
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(grpc_auth.UnaryServerInterceptor(authenticate)),
+		))
+
 	gsrv := grpc.NewServer(opts...)
 	srv, err := newgrpcServer(config)
 	if err != nil {
 		return nil, err
 	}
+
 	api.RegisterLogServer(gsrv, srv)
 	return gsrv, nil
 }
-func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
-	*api.ProduceResponse, error) {
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+
+	if err := s.Authorizer.Authorize(subject(ctx), objectWildcard, produceAction); err != nil {
+		return nil, err
+	}
+
 	offset, err := s.CommitLog.Append(req.Record)
 	if err != nil {
 		return nil, err
@@ -46,8 +71,11 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	return &api.ProduceResponse{Offset: offset}, nil
 }
 
-func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
-	*api.ConsumeResponse, error) {
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	if err := s.Authorizer.Authorize(subject(ctx), objectWildcard, consumeAction); err != nil {
+		return nil, err
+
+	}
 	record, err := s.CommitLog.Read(req.Offset)
 	if err != nil {
 		return nil, err
@@ -78,10 +106,7 @@ func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
 // will stream every record that follows—even records that aren’t in the log yet!
 // When the server reaches the end of the log, the server will wait until someone
 // appends a record to the log and then continue streaming records to the client.
-func (s *grpcServer) ConsumeStream(
-	req *api.ConsumeRequest,
-	stream api.Log_ConsumeStreamServer,
-) error {
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
 	for {
 		select {
 		case <-stream.Context().Done():
@@ -107,3 +132,33 @@ type CommitLog interface {
 	Append(*api.Record) (uint64, error)
 	Read(uint64) (*api.Record, error)
 }
+
+type Authorizer interface {
+	Authorize(subject, object, action string) error
+}
+
+// authenticate(context.Context) function is an interceptor that reads the subject
+// out of the client’s cert and writes it to the RPC’s context.
+func authenticate(ctx context.Context) (context.Context, error) {
+
+	peer, ok := peer.FromContext(ctx)
+	if !ok {
+		return ctx, status.New(codes.Unknown, "couldn't find peer info").Err()
+	}
+	if peer.AuthInfo == nil {
+		return context.WithValue(ctx, subjectContextKey{}, ""), nil
+	}
+
+	tlsInfo := peer.AuthInfo.(credentials.TLSInfo)
+	subject := tlsInfo.State.VerifiedChains[0][0].Subject.CommonName
+	ctx = context.WithValue(ctx, subjectContextKey{}, subject)
+	return ctx, nil
+}
+
+// subject(context.Context) function returns the client’s cert’s subject so
+// we can identify a client and check their access.
+func subject(ctx context.Context) string {
+	return ctx.Value(subjectContextKey{}).(string)
+}
+
+type subjectContextKey struct{}
