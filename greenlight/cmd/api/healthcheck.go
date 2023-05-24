f8107556c5eb922b86e11a909d35f4f123dd2b41
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-24 20:08:45 +0300

    json encoding, add movie struct

--- greenlight/cmd/api/healthcheck.go
+++ greenlight/cmd/api/healthcheck.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// we're using a raw string literal (enclosed with backticks) We also use the %q verb to
+	// wrap the interpolated values in double-quotes.
+	// js := `{"status": "available", "environment": %q, "version": %q}`
+	// js = fmt.Sprintf(js, app.config.env, version)
+
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ greenlight/cmd/api/helpers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type envelope map[string]interface{}
+
+// readIDParam reads and convert the id from the request context
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// the value returned by ByName() is always a string. So we try to convert it to a base 10 integer
+	// (with a bit size of 64). If the parameter couldn't be converted
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+
+	// NOTE: that it's OK if the provided header map is nil. Go doesn't throw an error
+	// if you try to range over (or generally, read from) a nil map.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
