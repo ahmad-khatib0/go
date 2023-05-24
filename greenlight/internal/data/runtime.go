0755d16603faaf7dfa75c1d50d23f4f5a614cd22
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-24 22:17:04 +0300

    Custom JSON Decoding

--- greenlight/internal/data/runtime.go
+++ greenlight/internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // Runtime has the underlying type int32 (the same as our Movie struct field).
 type Runtime int32
@@ -23,4 +27,29 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	// Convert the quoted string value to a byte slice and return it.
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Split the string to isolate the part containing the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Otherwise, parse the string containing the number into an int32. Again, if this
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Convert the int32 to a Runtime type and assign this to the receiver. Note that we
+	// use the * operator to deference the receiver (which is a pointer to a Runtime
+	// type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+	return nil
 }
