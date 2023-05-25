084925e1d00322bcafc4c05b9b2c1111032c3361
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-25 11:47:39 +0300

    optimistic locking pattern

--- greenlight/internal/data/movies.go
+++ greenlight/internal/data/movies.go
@@ -67,10 +67,11 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Update(movie *Movie) error {
+	// Add the 'AND version = $6' clause to the SQL query.
 	query := `UPDATE movies
-						SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-						WHERE id = $5
-						RETURNING version`
+							SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+							WHERE id = $5 AND version = $6
+							RETURNING version`
 
 	args := []interface{}{
 		movie.Title,
@@ -78,9 +79,22 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	// Execute the SQL query. If no matching row could be found, we know the movie
+	// version has changed (or the record has been deleted) and we return our custom ErrEditConflict error.
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
