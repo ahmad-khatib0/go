719622ea9d1dd614c7b4f5c253d03d568c196894
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-25 18:30:39 +0300

    Intercepting Shutdown Signals

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
 	"time"
 
@@ -71,21 +69,7 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		// ErrorLog:     log.New(logger, "", 0),
-	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
+	err = app.serve()
 	logger.PrintFatal(err, nil)
 }
 
---
+++ greenlight/cmd/api/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	go func() {
+		// We need to use a buffered channel here because signal.Notify() does not wait for a
+		// receiver to be available when sending a signal to the quit channel. If we had used a regular
+		// (non-buffered) channel here instead, a signal could be ‘missed’ if our quit channel is not
+		// ready to receive at the exact moment that the signal is sent. By using a buffered channel, we
+		// avoid this problem and ensure that we never miss a signal.
+		quit := make(chan os.Signal, 1)
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and relay them to the quit channel
+		// Any other signals will not be caught by signal.Notify() and will retain their default behavior.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		// Read the signal from the quit channel. This code will block until a signal is received.
+		s := <-quit
+
+		app.logger.PrintInfo("caught signal", map[string]string{"signal": s.String()})
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
+
+	app.logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.config.env,
+	})
+
+	return srv.ListenAndServe()
+}
