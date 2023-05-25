d390c364b597cf87d08923503e055e40aa617767
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-25 16:44:19 +0300

    Panic Recovery

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -68,6 +68,7 @@ func main() {
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		// ErrorLog:     log.New(logger, "", 0),
 	}
 
 	logger.PrintInfo("starting server", map[string]string{
---
+++ greenlight/cmd/api/middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// deferred function (which will always be run in the event of a panic as Go unwinds the stack).
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
