135b291cbc06a2a35d2d006c2a2dadd86d377565
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-05-25 22:18:21 +0300

    Registering a User

--- greenlight/internal/data/users.go
+++ greenlight/internal/data/users.go
@@ -35,7 +35,7 @@ var (
 
 func (m UserModel) Insert(user *User) error {
 	query := `INSERT INTO users (name, email, password_hash, activated)
-							VALUES ($1, $2, $3, $4) RETURNING id, created_at, version`
+							VALUES ($1, lower($2), $3, $4) RETURNING id, created_at, version`
 
 	args := []interface{}{user.Name, user.Email, user.Password.hash, user.Activated}
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
@@ -56,7 +56,7 @@ func (m UserModel) Insert(user *User) error {
 
 func (m UserModel) GetByEmail(email string) (*User, error) {
 	query := `SELECT id, created_at, name, email, password_hash, activated, version
-							FROM users WHERE email = $1`
+							FROM users WHERE email = lower($1)`
 
 	var user User
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
@@ -85,7 +85,7 @@ func (m UserModel) GetByEmail(email string) (*User, error) {
 
 func (m UserModel) Update(user *User) error {
 	query := `UPDATE users
-             SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+             SET name = $1, email = lower($2), password_hash = $3, activated = $4, version = version + 1
              WHERE id = $5 AND version = $6  RETURNING version`
 	args := []interface{}{
 		user.Name,
