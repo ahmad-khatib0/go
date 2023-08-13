5dae3e1b8684ba60e571c4e95382827fb1e42a0b
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-08-14 00:03:32 +0300

    metadata service controller and handler scaffolding

---
+++ microservice/movies/metadata/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ahmad-khatib0/go/microservice/movies/metadata/internal/controller/metadata"
+	httphandler "github.com/ahmad-khatib0/go/microservice/movies/metadata/internal/handler/http"
+	"github.com/ahmad-khatib0/go/microservice/movies/metadata/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Starting the movie metadata service")
+	repo := memory.New()
+	ctrl := metadata.New(repo)
+	h := httphandler.New(ctrl)
+
+	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
+}
---
+++ microservice/movies/metadata/internal/controller/metadata/controller.go
@@ -0,0 +1,36 @@
+package metadata
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ahmad-khatib0/go/microservice/movies/metadata/internal/repository"
+	model "github.com/ahmad-khatib0/go/microservice/movies/metadata/pkg"
+)
+
+// ErrNotFound is returned when a requested record is not found.
+var ErrNotFound = errors.New("not found")
+
+type metadataRepository interface {
+	Get(ctx context.Context, id string) (*model.Metadata, error)
+}
+
+// Controller defines a metadata service controller.
+type Controller struct {
+	repo metadataRepository
+}
+
+// New creates a metadata service controller.
+func New(repo metadataRepository) *Controller {
+	return &Controller{repo}
+}
+
+// Get returns movie metadata by id.
+func (c *Controller) Get(ctx context.Context, id string) (*model.Metadata, error) {
+	res, err := c.repo.Get(ctx, id)
+	if err != nil && errors.Is(err, repository.ErrNotFound) {
+		return nil, ErrNotFound
+	}
+
+	return res, nil
+}
