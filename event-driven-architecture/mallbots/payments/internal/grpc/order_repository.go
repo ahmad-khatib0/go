329d8831534e125404ae8c3af5ef39eed8225f30
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-09-18 20:41:41 +0300

    payments srv internal

---
+++ event-driven-architecture/mallbots/payments/internal/grpc/order_repository.go
@@ -0,0 +1,27 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/ahmad-khatib0/go/event-driven-architecture/mallbots/ordering/orderingpb"
+	"github.com/ahmad-khatib0/go/event-driven-architecture/mallbots/payments/internal/application"
+	"google.golang.org/grpc"
+)
+
+type OrderRepository struct {
+	client orderingpb.OrderingServiceClient
+}
+
+var _ application.OrderRepository = (*OrderRepository)(nil)
+
+func NewOrderRepository(conn *grpc.ClientConn) OrderRepository {
+	return OrderRepository{client: orderingpb.NewOrderingServiceClient(conn)}
+}
+
+func (r OrderRepository) Complete(ctx context.Context, invoiceID, orderID string) error {
+	_, err := r.client.CompleteOrder(ctx, &orderingpb.CompleteOrderRequest{
+		Id:        orderID,
+		InvoiceId: invoiceID,
+	})
+	return err
+}
