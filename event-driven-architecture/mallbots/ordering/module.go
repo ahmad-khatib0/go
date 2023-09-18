fce8352cc1f238c071e2bb2a16fd474cdaf47ffb
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-09-18 14:46:07 +0300

    scaffold the ordering srv

---
+++ event-driven-architecture/mallbots/ordering/module.go
@@ -0,0 +1,14 @@
+package ordering
+
+import (
+	"context"
+
+	"github.com/ahmad-khatib0/go/event-driven-architecture/mallbots/internal/monolith"
+)
+
+type Module struct{}
+
+func (Module) Startup(ctx context.Context, mono monolith.Monolith) error {
+
+	return nil
+}
