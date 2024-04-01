59adc55be0113633108b9a100653cc7a77110c7c
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2024-04-01 08:54:56 +0300

    json helpers

---
+++ websockets/chat/pkg/utils/json.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"bytes"
+	"errors"
+	"io"
+)
+
+func (u *Utils) JsonLineAndCharErr(offset int64, pay []byte) (int, int, error) {
+	if offset < 0 {
+		return -1, -1, errors.New("offset value cannot be negative")
+	}
+
+	br := bytes.NewReader(pay)
+	// Count lines and characters.
+	lnum := 1
+	cnum := 0
+
+	// number of consumed bytes
+	var count int64
+	for {
+		ch, size, err := br.ReadRune()
+		if err == io.EOF {
+			return -1, -1, errors.New("offset value too large")
+		}
+
+		count += int64(size)
+		if ch == '\n' {
+			lnum++
+			cnum = 0
+		} else {
+			cnum++
+		}
+
+		if count >= offset {
+			break
+		}
+	}
+
+	return lnum, cnum, nil
+}
