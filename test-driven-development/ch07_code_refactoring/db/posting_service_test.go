2d49384a9939b123ea2dde04306f94ca7e3dabfa
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-02-08 17:10:13 +0300

    code refactoring, error handling

---
+++ test-driven-development/ch07_code_refactoring/db/posting_service_test.go
@@ -0,0 +1,18 @@
+package db_test
+
+import (
+	"testing"
+
+	"github.com/ahmad-khatib0/go/test-driven-development/ch07_code_refactoring/db"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewOrder(t *testing.T) {
+	ps := db.NewPostingService()
+	b := db.Book{
+		ID: uuid.New().String(),
+	}
+	err := ps.NewOrder(b)
+	assert.Nil(t, err)
+}
